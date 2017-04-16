3768c25f9aad88b7802b7fb14d92f5e14538a201
Akriti Bilala <abilala@ncsu.edu>
2017-04-15 22:25:25 -0400

    bug fix

--- src/Receiver.java
+++ src/Receiver.java
@@ -22,6 +22,18 @@ public class Receiver {
 		this.fileName = fileName;
 		this.probability = probability;
 	}
+	/* public static String trimTrailingBlanks( String str)
+	  {
+	    if( str == null)
+	      return null;
+	    int len = str.length();
+	    for( ; len > 0; len--)
+	    {
+	      if( ! Character.isWhitespace( str.charAt( len - 1)))
+	         break;
+	    }
+	    return str.substring( 0, len);
+	  }*/
 
 	public static void main(String args[]) throws IOException {
 		int index = 0;
@@ -32,7 +44,6 @@ public class Receiver {
 		String fileName = sc.next();
 		System.out.println("Enter the probability: ");
 		double probability = sc.nextDouble();
-		double r = Math.random();
 		Receiver rec = new Receiver(portNumber, fileName, probability);
 		// BufferedWriter out = new BufferedWriter(new FileWriter(args[1],
 		// true));
@@ -50,7 +61,9 @@ public class Receiver {
 				System.out.println("here");
 				DatagramPacket senderData = new DatagramPacket(recieveData, recieveData.length);
 				server.receive(senderData);
+				System.out.println("rec len: "+senderData.getLength());
 				String receive = new String(senderData.getData());
+				receive = receive.substring(0, senderData.getLength());
 				String seq = receive.substring(0, 32);
 				int s = 0;
 				for (int i = 0; i < seq.length(); i++) {
@@ -60,32 +73,49 @@ public class Receiver {
 					}
 				}
 				String checksum = receive.substring(32, 48);
+				int c = 0;
+				for (int i = 0; i < checksum.length(); i++) {
+
+					if (seq.charAt(i) == '1') {
+						s = s + (int) Math.pow(2, checksum.length() - 1 - i);
+					}
+				}
 				String type = receive.substring(48, 64);
 				String data = receive.substring(64, receive.length());
-				System.out.println("seq: "+seq);
-				System.out.println("checksum: "+checksum);
-				System.out.println("type: "+type);
-				System.out.println("data: "+data);
+				System.out.println("seq: " + seq);
+				System.out.println("checksum: " + checksum);
+				System.out.println("type: " + type);
+				//String right = trimTrailingBlanks(data);
+				
+				System.out.println("data: " + data+" with length: "+data.length());
 				if (type.equals("1111111111111111")) {
 					flag = 1;
 				}
-//				if (r <= rec.probability) {
-//					System.out.println("Packet Loss Sequence Number =" + s);
-//				} else 
-					if ((rec.createChecksum(data.getBytes(), checksum.getBytes()).equals("1111111111111111"))) {
-						System.out.println("checksum is ok");
+
+				double r = Math.random();
+				System.out.println("random: "+r);
+				 if (r <= rec.probability) {
+				 System.out.println("Packet Loss Sequence Number =" + s);
+				 } else{
+//				if (rec.receive(data, c) == 0) {
+					System.out.println("checksum is ok");
 					if (s == index) {
+						System.out.println("Sending ack for: "+s);
 						String ack = rec.createACK(s);
 						InetAddress addr = senderData.getAddress();
 						int port = senderData.getPort();
-						byte[] b = ack.getBytes();
+						byte[] b;
+						b = ack.getBytes();
+//						for(int i=0;i<b.length;i++)
+//							System.out.print(b[i]+" ");
+//						System.out.println();
 						DatagramPacket sendpacket = new DatagramPacket(b, b.length, addr, port);
 						server.send(sendpacket);
 						incomingdata.write(data.getBytes());
 						index++;
 					}
-
-				}
+				 }
+//				}
 
 				// incomingdata.write(senderData.getData());
 			} catch (Exception e) {
@@ -93,14 +123,16 @@ public class Receiver {
 				System.err.println(e);
 			}
 		}
-		 FileOutputStream out = new FileOutputStream(fileName);
-		 incomingdata.writeTo(out);
-		 out.close();
-		 server.close();
+		FileOutputStream out = new FileOutputStream(fileName);
+		incomingdata.writeTo(out);
+		out.close();
+		server.close();
 	}
 
 	public String createACK(int seqno) {
 		String seq = Integer.toBinaryString(seqno);
+		for(int i=seq.length();i<32;i++)
+			seq = "0" + seq;
 		String check = "0000000000000000";
 		String field = "1010101010101010";
 		String ack = seq + check + field;
@@ -109,27 +141,104 @@ public class Receiver {
 
 	}
 
-	public String createChecksum(byte[] segment, byte[] check) {
-		int length = segment.length;
-		int Word, Checksum = 0;
-
-		while (length > 0) // len = Total num of bytes
-		{
-			int i = 0;
-			Word = ((segment[i] << 8) + segment[i + 1]) + Checksum + ((check[i] << 8) + check[i + 1]);
-
-			Checksum = Word & 0x0FFFF; // Discard the carry if any
-
-			Word = (Word >> 16); // Keep the carryout for value exceeding 16 Bit
-
-			Checksum = Word + Checksum; // Add the carryout if any
-
-			length -= 2; // decrease by 2 for 2 byte boundaries
-			i += 2;
+	public int generateChecksum(String s) {
+		System.out.println("checksum for " + s);
+		String hex_value = new String();
+		// 'hex_value' will be used to store various hex values as a string
+		int x, i, checksum = 0;
+		// 'x' will be used for general purpose storage of integer values
+		// 'i' is used for loops
+		// 'checksum' will store the final checksum
+		for (i = 0; i < s.length() - 2; i = i + 2) {
+			x = (int) (s.charAt(i));
+			hex_value = Integer.toHexString(x);
+			x = (int) (s.charAt(i + 1));
+			hex_value = hex_value + Integer.toHexString(x);
+			// Extract two characters and get their hexadecimal ASCII values
+			System.out.println(s.charAt(i) + "" + s.charAt(i + 1) + " : " + hex_value);
+			x = Integer.parseInt(hex_value, 16);
+			// Convert the hex_value into int and store it
+			checksum += x;
+			// Add 'x' into 'checksum'
 		}
+		if (s.length() % 2 == 0) {
+			// If number of characters is even, then repeat above loop's steps
+			// one more time.
+			x = (int) (s.charAt(i));
+			hex_value = Integer.toHexString(x);
+			x = (int) (s.charAt(i + 1));
+			hex_value = hex_value + Integer.toHexString(x);
+			System.out.println(s.charAt(i) + "" + s.charAt(i + 1) + " : " + hex_value);
+			x = Integer.parseInt(hex_value, 16);
+		} else {
+			// If number of characters is odd, last 2 digits will be 00.
+			x = (int) (s.charAt(i));
+			hex_value = "00" + Integer.toHexString(x);
+			x = Integer.parseInt(hex_value, 16);
+			System.out.println(s.charAt(i) + " : " + hex_value);
+		}
+		checksum += x;
+		// Add the generated value of 'x' from the if-else case into 'checksum'
+		hex_value = Integer.toHexString(checksum);
+		// Convert into hexadecimal string
+		if (hex_value.length() > 4) {
+			// If a carry is generated, then we wrap the carry
+			int carry = Integer.parseInt(("" + hex_value.charAt(0)), 16);
+			// Get the value of the carry bit
+			hex_value = hex_value.substring(1, 5);
+			// Remove it from the string
+			checksum = Integer.parseInt(hex_value, 16);
+			// Convert it into an int
+			checksum += carry;
+			// Add it to the checksum
+		}
+		checksum = this.generateComplement(checksum);
+		// Get the complement
+		System.out.println("data Checksum: " + Integer.toBinaryString(checksum));
+		return checksum;
+	}
 
-		Checksum = ~Checksum;
-		return Integer.toBinaryString(Checksum);
+	public int receive(String s, int checksum) {
+		int generated_checksum = this.generateChecksum(s);
+		// Calculate checksum of received data
+		generated_checksum = this.generateComplement(generated_checksum);
+		System.out.println("checksum comp: " + Integer.toBinaryString(generated_checksum));
+		// Then get its complement, since generated checksum is complemented
+		int syndrome = generated_checksum + checksum;
+		// Syndrome is addition of the 2 checksums
+		syndrome = this.generateComplement(syndrome);
+		// It is complemented
+		System.out.println("Syndrome = " + Integer.toHexString(syndrome));
+		return syndrome;
+	}
 
+	public int generateComplement(int checksum) {
+		// Generates 15's complement of a hexadecimal value
+		checksum = Integer.parseInt("FFFF", 16) - checksum;
+		return checksum;
 	}
 }
+// public String createChecksum(byte[] segment, byte[] check) {
+// int length = segment.length;
+// int Word, Checksum = 0;
+//
+// while (length > 0) // len = Total num of bytes
+// {
+// int i = 0;
+// Word = ((segment[i] << 8) + segment[i + 1]) + Checksum + ((check[i] << 8) +
+// check[i + 1]);
+//
+// Checksum = Word & 0x0FFFF; // Discard the carry if any
+//
+// Word = (Word >> 16); // Keep the carryout for value exceeding 16 Bit
+//
+// Checksum = Word + Checksum; // Add the carryout if any
+//
+// length -= 2; // decrease by 2 for 2 byte boundaries
+// i += 2;
+// }
+//
+// Checksum = ~Checksum;
+// return Integer.toBinaryString(Checksum);
+//
+// }
--- src/Sender.java
+++ src/Sender.java
@@ -24,87 +24,158 @@ public class Sender {
 	byte[] data;
 	ArrayList<byte[]> segments;
 
-	public Sender(String hostName, int portNumber, String fileName, int windowSize, int mss, InetAddress ip) throws IOException {
+	public Sender(String hostName, int portNumber, String fileName, int windowSize, int mss, InetAddress ip)
+			throws IOException {
 		this.hostName = hostName;
 		this.portNumber = portNumber;
 		this.fileName = fileName;
 		this.windowSize = windowSize;
 		this.mss = mss;
-		this.ip=ip;
+		this.ip = ip;
 		Path file = Paths.get(fileName);
 		this.data = Files.readAllBytes(file);
-		this.NoOfPackets = data.length / this.mss;
+		this.NoOfPackets = (int) Math.ceil((double)data.length / this.mss);
 		int j = 0;
 		int k = mss;
 		segments = new ArrayList<>();
-		System.out.println("Data length: "+data.length);
-		System.out.println("Packets: "+this.NoOfPackets);
-		System.out.println("mss: "+this.mss);
+		System.out.println("Data length: " + data.length);
+		System.out.println("Packets: " + this.NoOfPackets);
+		System.out.println("mss: " + this.mss);
 		for (int i = 0; i < this.NoOfPackets; i++) {
 			byte[] segmentSize = new byte[this.mss];
-			if(k>data.length)
-				k=data.length;
-			segmentSize = Arrays.copyOfRange(this.data, j, k);
+			for (int l = 0; l < this.mss; l++) {
+				if ((l + j) < this.data.length)
+					segmentSize[l] = this.data[l + j];
+			}
 			j = j + mss;
-			k = k + mss;
-			System.out.println("size "+segmentSize.length);
+			System.out.println("size " + segmentSize.length);
 			this.segments.add(i, segmentSize);
 			System.out.println(this.segments);
 		}
 	}
-	
 
-	public String createChecksum(byte[] segment) {
-		int length = segment.length;
-		int Word, Checksum = 0;
-
-		int i = 0;
-		while (length > 0 && i<segment.length-1) // len = Total num of bytes
-		{
-			Word = ((segment[i] << 8) + segment[i + 1]) + Checksum;
-
-			Checksum = Word & 0x0FFFF; // Discard the carry if any
-
-			Word = (Word >> 16); // Keep the carryout for value exceeding 16 Bit
-
-			Checksum = Word + Checksum; // Add the carryout if any
-
-			length -= 2; // decrease by 2 for 2 byte boundaries
-			i += 2;
+	public String generateChecksum(String s) {
+		System.out.println("checksum for " + s);
+		String hex_value = new String();
+		// 'hex_value' will be used to store various hex values as a string
+		int x, i, checksum = 0;
+		// 'x' will be used for general purpose storage of integer values
+		// 'i' is used for loops
+		// 'checksum' will store the final checksum
+		for (i = 0; i < s.length() - 2; i = i + 2) {
+			x = (int) (s.charAt(i));
+			hex_value = Integer.toHexString(x);
+			x = (int) (s.charAt(i + 1));
+			hex_value = hex_value + Integer.toHexString(x);
+			// Extract two characters and get their hexadecimal ASCII values
+			System.out.println(s.charAt(i) + "" + s.charAt(i + 1) + " : " + hex_value);
+			x = Integer.parseInt(hex_value, 16);
+			// Convert the hex_value into int and store it
+			checksum += x;
+			// Add 'x' into 'checksum'
 		}
-
-		Checksum = ~Checksum;
-		return Integer.toBinaryString(Checksum);
-
+		if (s.length() % 2 == 0) {
+			// If number of characters is even, then repeat above loop's steps
+			// one more time.
+			x = (int) (s.charAt(i));
+			hex_value = Integer.toHexString(x);
+			x = (int) (s.charAt(i + 1));
+			hex_value = hex_value + Integer.toHexString(x);
+			System.out.println(s.charAt(i) + "" + s.charAt(i + 1) + " : " + hex_value);
+			x = Integer.parseInt(hex_value, 16);
+		} else {
+			// If number of characters is odd, last 2 digits will be 00.
+			x = (int) (s.charAt(i));
+			hex_value = "00" + Integer.toHexString(x);
+			x = Integer.parseInt(hex_value, 16);
+			System.out.println(s.charAt(i) + " : " + hex_value);
+		}
+		checksum += x;
+		// Add the generated value of 'x' from the if-else case into 'checksum'
+		hex_value = Integer.toHexString(checksum);
+		// Convert into hexadecimal string
+		if (hex_value.length() > 4) {
+			// If a carry is generated, then we wrap the carry
+			int carry = Integer.parseInt(("" + hex_value.charAt(0)), 16);
+			// Get the value of the carry bit
+			hex_value = hex_value.substring(1, 5);
+			// Remove it from the string
+			checksum = Integer.parseInt(hex_value, 16);
+			// Convert it into an int
+			checksum += carry;
+			// Add it to the checksum
+		}
+		checksum = this.generateComplement(checksum);
+		// Get the complement
+		return Integer.toBinaryString(checksum);
 	}
-	public void lastPacket(DatagramSocket s)
-	{
+
+	public int generateComplement(int checksum) {
+		// Generates 15's complement of a hexadecimal value
+		checksum = Integer.parseInt("FFFF", 16) - checksum;
+		return checksum;
+	}
+
+	// public String createChecksum(byte[] segment) {
+	// int length = segment.length;
+	// int Word, Checksum = 0;
+	//
+	// int i = 0;
+	// while (length > 0 && i<segment.length-1) // len = Total num of bytes
+	// {
+	// Word = ((segment[i] << 8) + segment[i + 1]) + Checksum;
+	//
+	// Checksum = Word & 0x0FFFF; // Discard the carry if any
+	//
+	// Word = (Word >> 16); // Keep the carryout for value exceeding 16 Bit
+	//
+	// Checksum = Word + Checksum; // Add the carryout if any
+	//
+	// length -= 2; // decrease by 2 for 2 byte boundaries
+	// i += 2;
+	// }
+	//
+	// Checksum = ~Checksum;
+	// return Integer.toBinaryString(Checksum);
+	//
+	// }
+	public void lastPacket(DatagramSocket s) {
 		byte[] lastpacket = "1111111111111111111111111111111111111111111111111111111111111111000000".getBytes();
-		
-		DatagramPacket lastPacket = new DatagramPacket(lastpacket, lastpacket.length,this.ip,this.portNumber);
+
+		DatagramPacket lastPacket = new DatagramPacket(lastpacket, lastpacket.length, this.ip, this.portNumber);
 		try {
 			s.send(lastPacket);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}// remove seqno
-	public String createheader(int seqno, ArrayList<byte[]> segments, int index) {
+
+	public String createheader(int index, byte[] segment) {
 		String seq = Integer.toBinaryString(index);
-		String check = this.createChecksum(segments.get(index));
+		String check = this.generateChecksum(segment.toString());
 		String field = "0101010101010101";
-		for(int i= seq.length();i<32;i++)
+		for (int i = seq.length(); i < 32; i++)
 			seq = "0" + seq;
 		String header = seq + check + field;
-		System.out.println("seq:"+seq+" check: "+check+" field: "+field);
+		System.out.println("seq:" + seq + " check: " + check + " field: " + field);
 		return header;
 
 	}
 
 	public String checkACK(byte[] arr) {
-		String ACKPacket = Arrays.toString(arr);
+		String ACKPacket = "";
+		for(int i=0;i<64;i++){
+			if(arr[i]==48)
+				ACKPacket = ACKPacket + "0";
+			else
+				ACKPacket = ACKPacket +  "1";
+		}
+		System.out.println("string ack: "+ACKPacket);
 		String sequence = ACKPacket.substring(0, 32);
+		System.out.println("sequence: "+sequence);
+		System.out.println("type: "+ACKPacket.substring(48, 64));
 		if (ACKPacket.substring(48, 64).equals("1010101010101010")) {
 			return sequence;
 		} else {
@@ -116,6 +187,7 @@ public class Sender {
 	public int getpacket(DatagramPacket datapacket) {
 		int binToDec = -1;
 		String s = this.checkACK(datapacket.getData());
+		System.out.println("check ack: "+s);
 		if (s != null) {
 			binToDec = 0;
 			int j = s.length() - 1;
@@ -143,33 +215,33 @@ public class Sender {
 		System.out.println("Enter the MSS: ");
 		int mss = sc.nextInt();
 		InetAddress IPAddress = InetAddress.getByName(hostName);
-		Sender sender = new Sender(hostName, portNumber, fileName, windowSize, mss,IPAddress);
+		Sender sender = new Sender(hostName, portNumber, fileName, windowSize, mss, IPAddress);
 		DatagramSocket client = new DatagramSocket();
 
-		int seq = 0;
+		int seq = -1;
 		int startIndex = 0, i = 0, count = 0;
 		while (startIndex < sender.NoOfPackets) {
-
-			while (i < sender.windowSize) {
-				String head = sender.createheader(count, sender.segments, startIndex);
+			while (i < sender.windowSize && startIndex < sender.NoOfPackets) {
+				String head = sender.createheader(startIndex, sender.segments.get(startIndex));
 				byte[] headbytes = head.getBytes();
 				byte[] databytes = sender.segments.get(startIndex);
 				byte[] finalPacket = new byte[headbytes.length + sender.mss];
 				for (int k = 0; k < headbytes.length; k++) {
 					finalPacket[k] = headbytes[k];
 				}
-				for (int j = headbytes.length, k=0; j < headbytes.length + sender.mss; j++, k++) {
+				for (int j = headbytes.length, k = 0; j < headbytes.length + sender.mss; j++, k++) {
 					finalPacket[j] = databytes[k];
 				}
 				DatagramPacket send = new DatagramPacket(finalPacket, finalPacket.length, IPAddress, sender.portNumber);
 				client.send(send);
-				System.out.println("sent:"+startIndex);
+				System.out.println("sent:" + startIndex);
 				i++;
 				startIndex++;
 				count++;
 			}
-			System.out.println("Receiving with i:"+i+" index: "+startIndex);
-			byte[] receiveData = new byte[2048];
+			System.out.println("Receiving with i:" + i + " index: " + startIndex);
+			byte[] receiveData = new byte[1024];
+			int indexCopy = startIndex;
 			DatagramPacket receivePckt = new DatagramPacket(receiveData, receiveData.length);
 			try {
 				client.setSoTimeout(1000);
@@ -178,10 +250,11 @@ public class Sender {
 					seq = sender.getpacket(receivePckt); // getting the sequence
 															// no of the
 															// received packet
-					System.out.println("ACK received with seq: "+seq);
-					if (seq == startIndex - 1) { // if last acknowledgement is
+					System.out.println("ACK received with seq: " + seq);
+					if (seq == indexCopy - 1) { // if last acknowledgement is
 													// received
 						i = 0;
+						startIndex = indexCopy;
 						break;
 					} else if (seq != -1) { // if last is not received, then
 											// change window size and retransmit
@@ -193,12 +266,12 @@ public class Sender {
 				}
 			} catch (SocketTimeoutException s) {
 				System.out.println("Timeout, sequence number = " + seq);
-				startIndex = seq;
+				startIndex = seq+1;
 				i = 0;
 			}
 
 		}
 		sender.lastPacket(client);
 	}
-	
+
 }
